cdb749e59aa3b904b82ff1eb18a6d146ee6ca4d1
Xiaoming Liu <xiaoming.liu@autodesk.com>
2017-02-26 23:59:45 +0800

    Update search and time difference function
    Update search and time difference function

--- src/com/TestServlet/HTTPRequest.java
+++ src/com/TestServlet/HTTPRequest.java
@@ -2,16 +2,39 @@ package com.TestServlet;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.net.Authenticator;
 import java.net.HttpURLConnection;
+import java.net.PasswordAuthentication;
 import java.net.URL;
+
+import org.apache.commons.codec.binary.Base64;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 public class HTTPRequest {
 	private final String USER_AGENT = "Mozilla/5.0";
+	private String password = new String();
+	private String username = new String();
+	
+	public HTTPRequest(){}
+	
+	public HTTPRequest(String uname, String pass){
+		password = pass;
+		username = uname;
+	}
+
 	
 	public String sendGet(String urladdr) throws Exception {
-
+		String authString = username + ":" + password;
+		System.out.println("auth string: " + authString);
+		byte[] authEncBytes = Base64.encodeBase64(authString.getBytes());
+		String authStringEnc = new String(authEncBytes);
+		System.out.println("Base64 encoded auth string: " + authStringEnc);
 		URL obj = new URL(urladdr);
 		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		con.setRequestProperty("Authorization", "Basic " + authStringEnc);
+
 
 		// optional default is GET
 		con.setRequestMethod("GET");
@@ -23,17 +46,16 @@ public class HTTPRequest {
 		int responseCode = con.getResponseCode();
 		System.out.println("\nSending 'GET' request to URL : " + urladdr);
 		System.out.println("Response Code : " + responseCode);
-
 		BufferedReader in = new BufferedReader(
 		        new InputStreamReader(con.getInputStream()));
 		String inputLine;
 		StringBuffer response = new StringBuffer();
+		
 
 		while ((inputLine = in.readLine()) != null) {
 			response.append(inputLine);
 		}
 		in.close();
-
 		//print result
 		return response.toString();
 
--- src/com/TestServlet/ServletMain.java
+++ src/com/TestServlet/ServletMain.java
@@ -2,16 +2,18 @@ package com.TestServlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.google.gson.*;
 
-
-import java.net.*;
 
 
 /**
@@ -58,6 +60,8 @@ public class ServletMain extends HttpServlet {
 		String pass = request.getParameter("pass");
 		String index = request.getParameter("index");
 		String url;
+		Gson gson = new Gson();
+
 			
         PrintWriter writer = response.getWriter();
         
@@ -71,26 +75,46 @@ public class ServletMain extends HttpServlet {
         	
         }else{
         	
-        	Authenticator.setDefault(new Authenticator() {
-        		 @Override
-        		        protected PasswordAuthentication getPasswordAuthentication() {
-        		         return new PasswordAuthentication(
-        		   name, pass.toCharArray());
-        		        }
-        	});
-        	
-        	HTTPRequest newreq = new HTTPRequest();
+        	HTTPRequest newreq = new HTTPRequest(name, pass);
+        	String respStr = new String();
         	try {
-				newreq.sendGet("http://localhost:8090/jira/rest/api/2/issue/HSP-1");
+        		respStr = newreq.sendGet("https://dts-stg.autodesk.com/rest/api/2/issue/" + index);
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+    		JsonObject result = new JsonParser().parse(respStr).getAsJsonObject();
+    		String creationTime = result.get("fields").getAsJsonObject().get("created").toString();
+    		creationTime = creationTime.substring(1, creationTime.length()-1);
+    		String status = result.get("fields").getAsJsonObject().get("status").getAsJsonObject().get("statusCategory").getAsJsonObject().get("name").toString();
+    		String resolutiondate = new String();
+    		resolutiondate = result.get("fields").getAsJsonObject().get("resolutiondate").toString();
+    		DateFormat formater = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+
 
         	
 	        htmlResponse += "<h2>Your username is: " + name + "<br/>";      
 	        htmlResponse += "Your password is: " + pass + "<br/>"; 
 	        htmlResponse += "The entry you are looking into is: " + index + "<h2/>";
+	        htmlResponse += "<p>" + creationTime + "</p>";
+	        htmlResponse += "<p>" + status + "</p>";
+    		if(!resolutiondate.isEmpty() && resolutiondate!=null){
+    			try {
+    	    		resolutiondate = resolutiondate.substring(1, resolutiondate.length()-1);
+    		        htmlResponse += "<p>" + resolutiondate + "</p>";
+					Date start = formater.parse(creationTime);
+	    			Date end = formater.parse(resolutiondate);
+	    			long diffinmilles = end.getTime() - start.getTime();
+	    			//TimeUnit timeUnit = TimeUnit.DAYS;
+	    			long diffinhours = TimeUnit.HOURS.convert(diffinmilles, TimeUnit.MILLISECONDS);
+	    			htmlResponse += "<p>" + "The issues takes " + diffinhours + " hours to resolve" + "</p>";
+				} catch (ParseException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+    		}else{
+		        htmlResponse += "<p>" + "Unresolved" + "</p>";
+    		}
 	        htmlResponse += "</html>";
 	         
 	        // return response
